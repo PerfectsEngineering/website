78062cc9c1b84e5f63798098a4ad909f75c0dce9
omodara145 <omodara145@gmail.com>
2019-02-15 00:33:02 +0100

    Created The Header Section of the Website with Light/Dark Mode Toggle.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 // Main website javascript code here
+function toggleDarkLight() {
+    const body = document.querySelector('body');
+    const currentClass = body.className;
+    body.className = currentClass === "pe-dark" ? "pe-light" : "pe-dark";
+}
