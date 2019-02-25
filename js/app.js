1e354deb209ebffba3daf28309f41cdb8149df37
omodara145 <omodara145@gmail.com>
2019-02-25 23:57:15 +0100

    Website Header

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-// Main website javascript code here
+//Main website javascript code here
+document
+    .querySelector('.mode-toggle')
+    .addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+
 function toggleDarkLight() {
     const body = document.querySelector('body');
     const currentClass = body.className;
-    body.className = currentClass === "pe-dark" ? "pe-light" : "pe-dark";
+    body.className = currentClass === 'pe-dark' ? 'pe-light' : 'pe-dark';
+
+    localStorage.setItem('background', body.className);
 }
+
+if (localStorage.getItem('background') != null) {
+    document.body.className = localStorage.background;
+}
+
