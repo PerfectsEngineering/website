b4b1809d2a0a6384c4343d6a885a2f81eabc2234
Ismail Omodara <omodara145@gmail.com>
2019-02-28 05:02:13 +0100

    Website Header (#7)
    * Website Header
    
    * Moved app.js back to head and made it defer
    
    * Wrapped required functions inside DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
-// Main website javascript code here
+//Main website javascript code here
+document.addEventListener('DOMContentLoaded', function () {
+    document
+        .querySelector('.mode-toggle')
+        .addEventListener('click', function(event) {
+            event.preventDefault();
+        });
+})
+
 function toggleDarkLight() {
     const body = document.querySelector('body');
     const currentClass = body.className;
-    body.className = currentClass === "pe-dark" ? "pe-light" : "pe-dark";
+    body.className = currentClass === 'pe-dark' ? 'pe-light' : 'pe-dark';
+
+    localStorage.setItem('background', body.className);
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('background') != null) {
+        document.body.className = localStorage.background;
+    }
+})
+
