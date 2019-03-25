d9680967bc0e472e23c9d264df9d8091ef026336
Ismail Omodara <omodara145@gmail.com>
2019-03-25 10:34:31 +0100

    Feature: Newsletter Subscription (#13)
    * Added form for newsletter subscription, and reCaptcha for form verification
    
    * Newsletter subscription form

--- js/app.js
+++ js/app.js
@@ -23,3 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Set current year to footer
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// validateE
+function subscribeForNewsletter() {
+    const userEmail = document.getElementById('mce-EMAIL');
+    const validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+    if (validateEmail.test(userEmail.value)) {
+        document.getElementById("subscribe-form").submit();
+    } else {
+        document.getElementById("subscribe-form").classList.add('error');
+    }
+}
